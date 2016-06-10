a3ec5340bb5c18272081d2395b572a8c242c7e29
Ryan Mott <ryan.mott@vml.com>
2016-06-10 12:55:47 -0500

    simplify linestrings

--- src/gtfs_tables_makespatial.sql
+++ src/gtfs_tables_makespatial.sql
@@ -22,14 +22,18 @@ CREATE TABLE gtfs_shape_geoms (
 SELECT AddGeometryColumn('gtfs_shape_geoms', 'the_geom', 4326, 'LINESTRING', 2);
 
 -- Populate gtfs_shape_geoms
-INSERT INTO gtfs_shape_geoms
-SELECT shape.shape_id, ST_SetSRID(ST_MakeLine(shape.the_geom), 4326) As new_geom
+INSERT INTO gtfs_shape_geoms (shape_id, the_geom)
+  SELECT
+    shape.shape_id,
+    ST_SimplifyPreserveTopology(ST_SetSRID(ST_MakeLine(shape.the_geom), 4326), 0.00001) AS new_geom
   FROM (
-    SELECT shape_id, ST_MakePoint(shape_pt_lon, shape_pt_lat) AS the_geom
-    FROM gtfs_shapes 
-    ORDER BY shape_id, shape_pt_sequence
-  ) AS shape
-GROUP BY shape.shape_id;
+         SELECT
+           shape_id,
+           ST_MakePoint(shape_pt_lon, shape_pt_lat) AS the_geom
+         FROM gtfs_shapes
+         ORDER BY shape_id, shape_pt_sequence
+       ) AS shape
+  GROUP BY shape.shape_id;
 
 -- Create spatial index
 CREATE INDEX "gtfs_shape_geoms_the_geom_gist" ON "gtfs_shape_geoms" using gist ("the_geom");
